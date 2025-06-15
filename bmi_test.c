77ccfb034387287bb5136315d5c1e5335d4fce09
junyulai <2958199593@qq.com>
2025-06-15 20:47:56 +0800

    add input jiankangchuangtai

--- bmi_test.c
+++ bmi_test.c
@@ -4,5 +4,24 @@ int main(void)
     float weight = 18.8;
     float high = 16.5;
     float bmi = weight / ( high * high);
+    printf ("%f\n" , bmi);
+    if (bmi < 18.5)
+    {
+        printf("your bmi pianshou \n ");
+    }
+    if (bmi >= 18.5 && bmi <= 24.9)
+    {
+        printf("your bmi zhengchang");
+    }
+    if (bmi >24.9 && bmi <= 29.9)
+    {
+        printf ("your bmi changzhong");
+
+    }
+    if (bmi > 30)
+    {
+        printf("your bmi jingbuzheng");
+    }
+
 
 }
