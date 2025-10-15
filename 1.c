a73dcbf5786b8313c5c0eac57d4b9b6780af5231
laijunyu <2958199593@qq.com>
2025-10-15 22:23:43 +0800

    加入

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int n (int x,int y)
+{
+    printf("%d\n%d\n",x,y);
+}
+int main ()
+{
+    int a =2;
+    int b=3;
+    a+=3;
+    b++;
+    printf("a=%d b=%d",a,b);
+    n(a,b);
+    return 114514;
+    
+}
