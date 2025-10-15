2420736fd7ba3f049c6ca25c2c78128b656d3923
laijunyu <2958199593@qq.com>
2025-10-15 22:36:20 +0800

    fix bug

--- lainxi3_5.5.c
+++ lainxi3_5.5.c
@@ -3,7 +3,7 @@ int main ()
 {
     float a,b,h;//a为上底,bweixiadi,cweigao
     printf("请分别输入梯形的上底，下底，和高\n");
-    scanf("%f %f %f",&a ,&b ,&c);
+    scanf("%f %f %f",&a ,&b ,&h);
     float s = (a+b)*h/2;
     printf("梯形的面积为%f",s);
 
