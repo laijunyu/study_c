5e57df8607ddbbd9c0be018e86035b8718e662e0
laijunyu <2958199593@qq.com>
2025-10-15 22:58:52 +0800

    作业

---
+++ lianxi3_5.6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int score;
+    printf("请输入分数：");
+    scanf("%d", &score);
+
+    if (score >= 90)
+        printf("a");
+    else if (score >= 60)
+        printf("b");
+    else
+        printf("c");
+
+    return 0;
+}
