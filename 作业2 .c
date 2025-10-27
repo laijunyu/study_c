2741e589b1a38e2973651433cfdaf2ff75cb33f2
laijunyu <2958199593@qq.com>
2025-10-27 22:27:22 +0800

    增加注释

--- 作业2 .c
+++ 作业2 .c
@@ -4,25 +4,25 @@
 int main ()
 {
     int in;
-    printf("请输入：");
-    scanf(" %d",&in);
+    printf("请输入："); // 输入提示
+    scanf(" %d",&in);//输入
     if (in == 1){
-        printf("xinglihua");
+        printf("xinglihua");//进入第一个
     }
     else {
         if (in == 2){
-            printf("%d",18+63);
+            printf("%d",18+63);//进入第二个
         }
         else{
-            int in2,ram;
+            int in2,ram;//进入第三个游戏
             printf("caiziyouxikaishi\nqingshuru:");
-            srand((unsigned) time(0));
+            srand((unsigned) time(0));//设置随机数
             in2 = rand()%1 +100;
             while (in2 != in2)
             {
-                scanf("%d",&in2);
+                scanf("%d",&in2);//通过循环逐步确认随机数
                 if(in2 <ram){
-                    printf("caixiaole");
+                    printf("caixiaole");//猜大猜小提示
                 }
                 else{
                     printf("caidale");
