ec1b02ff1c198f0a1ccc3865e713d2ed9305feaf
laijunyu <2958199593@qq.com>
2025-11-11 23:43:07 +0800

    max

---
+++ fenxi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int max(int a,int b){
+    if (a<b)
+    {
+        return b;
+    }
+    int min (int a, int b){
+        if (a>b) return b;
+    }
+    int main (){
+        int i
+        cin>> i;
+        int w [i];
+        for(;i>0;i--){
+            cin>>a[i];
+        }
+
+    }
+    
+    
+}
